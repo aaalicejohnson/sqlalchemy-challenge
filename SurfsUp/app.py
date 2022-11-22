0686922a6656957a8c01be9de756da2f30446624
Alice Johnson <aaalicejohnson@gmail.com>
2022-11-22 16:20:33 -0500

    app code

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -35,8 +35,8 @@ def home():
         f"/api/v1.0/precipitation<br>"
         f"/api/v1.0/stations<br>"
         f"/api/v1.0/tobs<br>"
-        f"/api/v1.0/<start><br>"
-        f"/api/v1.0/<start>/<end><br>"
+        f"/api/v1.0/start=YYYY-MM-DD<br>"
+        f"/api/v1.0/start=YYYY-MM-DD/end=YYYY-MM-DD<br>"
     )
 
 #Routes
@@ -62,13 +62,21 @@ def precipitation():
 @app.route("/api/v1.0/stations")
 def stations():
     session = Session(engine)
-    results = session.query(Station.station).all()
+    results = session.query(Station).all()
+
+    station_list = []
+    for result in results: 
+        station_dict = {}
+        station_dict["id"] = result.id
+        station_dict["name"] = result.name
+        station_dict["latitude"] = result.latitude
+        station_dict["longitude"] = result.longitude
+        station_dict["elevation"] = result.elevation
+        station_list.append(station_dict)
 
     session.close( )
 
-    station_names = list(np.ravel(results))
-
-    return jsonify(station_names)
+    return jsonify(station_list)
 
 @app.route("/api/v1.0/tobs")
 def tobs(): 
@@ -96,6 +104,48 @@ def tobs():
 
     return jsonify(temp_data)
 
+@app.route("/api/v1.0/start=<start>")
+def start_date(start): 
+    session = Session(engine)
+
+    results = session.query(func.min(Measurement.tobs),
+        func.max(Measurement.tobs),
+        func.avg(Measurement.tobs)).\
+        filter(Measurement.date >= start).all()
+    
+    session.close()
+
+    temp_data = []
+    for tmin, tmax, tavg in results: 
+        temp_dict = {}
+        temp_dict["Temp Min"] = tmin
+        temp_dict["Temp Max"] = tmax 
+        temp_dict["Temp Avg"] = tavg
+        temp_data.append(temp_dict)
+
+        return jsonify(temp_data)
+
+@app.route("/api/v1.0/start=<start>/end=<end>")
+def dates(start, end): 
+    session = Session(engine)
+
+    results = session.query(func.min(Measurement.tobs),
+        func.max(Measurement.tobs),
+        func.avg(Measurement.tobs)).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    
+    session.close()
+
+    temp_data = []
+    for tmin, tmax, tavg in results: 
+        temp_dict = {}
+        temp_dict["Temp Min"] = tmin
+        temp_dict["Temp Max"] = tmax 
+        temp_dict["Temp Avg"] = tavg
+        temp_data.append(temp_dict)
+
+        return jsonify(temp_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
